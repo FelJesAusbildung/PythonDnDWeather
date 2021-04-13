1709228464accad67e32055e3e8d83077cce0e87
FelJesAusbildung <MCFeli@gmx.de>
2021-04-13 08:03:09 +0200

    now actually manipulates the group

--- grouper.py
+++ grouper.py
@@ -1,21 +1,17 @@
 from balancer import *
 
 
-def get_group(items, identifier):
+def get_group(items, identifier, key='chance'):
     group = []
     for item in items:
         if identifier.upper() in item['name']:
             group.append(item)
-    return group
+    return group, get_total_chance(group, key=key)
 
 
 if __name__ == "__main__":
     encounters = items_from_json("SailingEncounter.json")
-    randoms = get_group(encounters, "random")
-    randoms_sting = json.dumps(randoms, indent=2)
-    print(randoms_sting)
-    randoms, _ = balance(randoms, total=5, key='chance')
-    randoms_sting = json.dumps(randoms, indent=2)
-    print(randoms_sting)
-
+    randoms, randoms_total = get_group(encounters, "random")
+    print("-------------------------------------------------")
+    randoms = balance_with_output(randoms, total=100, key='chance')
     pass
