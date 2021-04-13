02503107d7d9537a6bd147b8795558cd97fe2df4
FelJesAusbildung <MCFeli@gmx.de>
2021-04-13 08:07:25 +0200

    added insert method

--- grouper.py
+++ grouper.py
@@ -9,9 +9,18 @@ def get_group(items, identifier, key='chance'):
     return group, get_total_chance(group, key=key)
 
 
+def insert(group, items):
+    for item in items:
+        for group_item in group:
+            if group_item['name'] == item['name']:
+                item = group_item
+    return items
+
+
 if __name__ == "__main__":
     encounters = items_from_json("SailingEncounter.json")
     randoms, randoms_total = get_group(encounters, "random")
     print("-------------------------------------------------")
     randoms = balance_with_output(randoms, total=100, key='chance')
+    insert(randoms, encounters)
     pass
